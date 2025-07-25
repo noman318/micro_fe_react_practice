26527ba1db1cfeeac855f98269ac1a0807f053d3
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2025-07-26 00:08:58 +0530

    Server setup completed with dummy data

---
+++ server/src/modules/products/product.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+
+import { ProductsController } from './product.controller';
+
+@Module({
+  controllers: [ProductsController],
+})
+export class ProductModule {}
